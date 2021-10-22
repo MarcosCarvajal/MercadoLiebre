d0a6d36c137cb64d483fef08e448ae24b2f698b4
Marcos Carvajal <mcarvajaldf@gmail.com>
2021-10-22 13:35:57 -0500

    resolviendo inconvenientes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', (req,res)=>{
     res.sendFile(path.resolve(__dirname,'./views/home.html'))
 })
 
-app.get('/register.html', (req,res)=>{
+app.get('/register', (req,res)=>{
     res.sendFile(path.resolve(__dirname,'./views/register.html'))
 })
 
